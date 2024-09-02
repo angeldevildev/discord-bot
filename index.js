bd6e45b840db6749f46d7803751bb88b4b3cffd6
mineshopdev <mineshop47@gmail.com>
2024-09-02 23:00:11 +0200

    Made the / commands global

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ dotenv.config();
 
 const TOKEN = process.env.TOKEN;
 const CLIENT_ID = process.env.CLIENT_ID;
-const GUILD_ID = process.env.GUILD_ID;
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
@@ -40,7 +39,7 @@ const rest = new REST({ version: '10' }).setToken(TOKEN);
 		console.log(`Started refreshing ${commands.length} application (/) commands.`);
 
 		const data = await rest.put(
-			Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID),
+			Routes.applicationCommands(CLIENT_ID),
 			{ body: commands },
 		);
 
