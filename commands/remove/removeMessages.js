1b583be655aa5f20ed0026869d2fd94fbb0815b1
mineshopdev <mineshop47@gmail.com>
2024-09-04 12:51:57 +0200

    Added removeMessages.js command

---
+++ commands/remove/removeMessages.js
@@ -0,0 +1,55 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('removemessages')
+        .setDescription('Remove a specified number of messages from the chat or from a specific user')
+        .addIntegerOption(option =>
+            option.setName('count')
+                .setDescription('Number of messages to delete')
+                .setRequired(true))
+        .addUserOption(option =>
+            option.setName('user')
+                .setDescription('The user whose messages will be deleted')
+                .setRequired(false)),
+    async execute(interaction) {
+        if (!interaction.member.permissions.has('MANAGE_MESSAGES')) {
+            return interaction.reply({ content: 'You do not have permission to manage messages.', ephemeral: true });
+        }
+
+        const count = interaction.options.getInteger('count');
+        const user = interaction.options.getUser('user');
+
+        if (count < 1 || count > 100) {
+            return interaction.reply({ content: 'Please enter a number between 1 and 100.', ephemeral: true });
+        }
+
+        try {
+            let messages;
+
+            if (user) {
+                messages = (await interaction.channel.messages.fetch({ limit: 100 }))
+                    .filter(msg => msg.author.id === user.id)
+                    .first(count);
+            } else {
+                messages = await interaction.channel.messages.fetch({ limit: count });
+            }
+
+            await interaction.channel.bulkDelete(messages, true);
+
+            const embed = new EmbedBuilder()
+                .setTitle('Messages Deleted')
+                .setColor('Red')
+                .setDescription(user 
+                    ? `Successfully deleted **${messages.size}** messages from **${user.tag}**.` 
+                    : `Successfully deleted **${messages.size}** messages from this channel.`)
+                .setFooter({ text: `Deleted by ${interaction.user.tag}` })
+                .setTimestamp();
+
+            interaction.reply({ embeds: [embed], ephemeral: true });
+        } catch (error) {
+            console.error(error);
+            interaction.reply({ content: 'An error occurred while trying to delete messages. Ensure messages are not older than 14 days.', ephemeral: true });
+        }
+    },
+};
